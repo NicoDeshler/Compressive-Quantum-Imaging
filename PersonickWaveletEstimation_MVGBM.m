96c432908b5726ef5551ea4f91b129232b6ccc45
Nico Deshler <nico.deshler@gmail.com>
2022-07-06 18:33:35 -0700

    had image display set to on

--- PersonickWaveletEstimation_MVGBM.m
+++ PersonickWaveletEstimation_MVGBM.m
@@ -141,7 +141,7 @@ set(groot,'defaultLegendInterpreter','latex');
 
 
 %% Make video objects
-make_videos = 1;
+make_videos = 0;
 if make_videos
     
     vid_a = VideoWriter(fullfile(save_dir,'TransformedParams.avi'));
